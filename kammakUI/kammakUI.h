cfbc086e17a41b6dc9c3324dab546f47337717fa
Damian Kolasinski <damian.kolasinski@appunite.com>
2018-05-01 23:33:44 +0200

    added kammakUI framework

---
+++ kammakUI/kammakUI.h
@@ -0,0 +1,19 @@
+//
+//  kammakUI.h
+//  kammakUI
+//
+//  Created by Damian Kolasiński on 01/05/2018.
+//  Copyright © 2018 DamianKolasinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for kammakUI.
+FOUNDATION_EXPORT double kammakUIVersionNumber;
+
+//! Project version string for kammakUI.
+FOUNDATION_EXPORT const unsigned char kammakUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kammakUI/PublicHeader.h>
+
+
