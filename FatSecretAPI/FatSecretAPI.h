e6f173635cb69dfa17ae964b6cba1c97a669c454
Damian Kolasinski <damian.kolasinski@appunite.com>
2018-05-12 13:29:16 +0200

    added moya

---
+++ FatSecretAPI/FatSecretAPI.h
@@ -0,0 +1,19 @@
+//
+//  FatSecretAPI.h
+//  FatSecretAPI
+//
+//  Created by Damian Kolasiński on 12/05/2018.
+//  Copyright © 2018 DamianKolasinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FatSecretAPI.
+FOUNDATION_EXPORT double FatSecretAPIVersionNumber;
+
+//! Project version string for FatSecretAPI.
+FOUNDATION_EXPORT const unsigned char FatSecretAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FatSecretAPI/PublicHeader.h>
+
+
